2179129ea43e5858aacc4634fe454eb5629d161c
KG5321 <koniaxd@gmail.com>
2020-10-19 23:59:01 +0200

    cleanup

--- pyvieslib/vies_validator.py
+++ pyvieslib/vies_validator.py
@@ -46,55 +46,48 @@ class ViesValidator:
         return result
 
     def _at_vies_check(self, vies_number: str) -> bool:
-        print("at")
         exp = "^U(\d{8})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
+
         return bool(r)
 
     def _be_vies_check(self, vies_number: str) -> bool:
-        print("be")
         exp = "^([0-1]\d{9})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
+
         return bool(r)
 
     def _bg_vies_check(self, vies_number: str) -> bool:
-        print("bg")
         exp = "^(\d{9,10})$/)$"
         r = re.search(exp, vies_number)
-        print(bool(r))
+
         return bool(r)
 
     def _ch_vies_check(self, vies_number: str) -> bool:
-        print("che")
+
         exp = "^(\d{9})(MWST|TVA|IVA)?$"
         r = re.search(exp, vies_number)
-        print(bool(r))
+
         return bool(r)
 
     def _cy_vies_check(self, vies_number: str) -> bool:
-        print("cy")
         exp = "^([0-59]\d{7}[A-Z])$"
         r = re.search(exp, vies_number)
-        print(bool(r))
+
         return bool(r)
 
     def _cz_vies_check(self, vies_number: str) -> bool:
-        # print("cz")
         exp = "^(\d{8,10})(\d{3})?$"
         r = re.search(exp, vies_number)
         if not bool(r):
-            print("regex 0")
             return False
-        cz_r0 = "^\d{8}$"  # CZ00008702
-        cz_r1 = "^[0-5][0-9][0|1|5|6][0-9][0-3][0-9]\d{3}$"  # CZ395601439
-        cz_r2 = "^6\d{8}$"  # CZ680447748
-        cz_r3 = "^\d{2}[0-3|5-8][0-9][0-3][0-9]\d{4}$"  # CZ5511061105
+        cz_r0 = "^\d{8}$"
+        cz_r1 = "^[0-5][0-9][0|1|5|6][0-9][0-3][0-9]\d{3}$"
+        cz_r2 = "^6\d{8}$"
+        cz_r3 = "^\d{2}[0-3|5-8][0-9][0-3][0-9]\d{4}$"
         digits = [int(i) for i in vies_number]
         weights = (8, 7, 6, 5, 4, 3, 2)
         if re.search(cz_r0, vies_number):
-            print("cz_r0")
             check_sum = 11 - sum(d * w for d, w in zip(digits, weights)) % 11
             if check_sum == 10:
                 check_sum = 10
@@ -102,12 +95,10 @@ class ViesValidator:
                 check_sum = 1
             return check_sum == digits[-1]
         if re.search(cz_r1, vies_number):
-            print("cz_r1")
             if int(vies_number[0:2]) > 62:
                 return False
             return True
         if re.search(cz_r2, vies_number):
-            print("cz_r2")
             check_sum = 0
             for i in range(0, 7):
                 check_sum += digits[i+1] * weights[i]
@@ -122,7 +113,6 @@ class ViesValidator:
                 return True
             return False
         if re.search(cz_r3, vies_number):
-            print("cz_r3")
             check_sum = int(vies_number[0:2]) + int(vies_number[2:4]) + int(
                 vies_number[4:6]) + int(vies_number[6:8]) + int(vies_number[8:])
             if (check_sum % 11 == 0) and (int(vies_number) % 11 == 0):
@@ -131,7 +121,6 @@ class ViesValidator:
         return False
 
     def _de_vies_check(self, vies_number: str) -> bool:
-        print("de")
         exp = "^([1-9]\d{8})$"
         product = 10
         check_sum = 0
@@ -152,14 +141,11 @@ class ViesValidator:
         return check_digit == digits[-1]
 
     def _dk_vies_check(self, vies_number: str) -> bool:
-        print("dk")
         exp = "^(\d{8})$$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _ee_vies_check(self, vies_number: str) -> bool:
-        print("ee")
         exp = "^(10\d{7})$"
         r = re.search(exp, vies_number)
         if not bool(r):
@@ -172,14 +158,11 @@ class ViesValidator:
         return check_sum == digits[-1]
 
     def _el_vies_check(self, vies_number: str) -> bool:
-        print("el")
         exp = "^(\d{9})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _es_vies_check(self, vies_number: str) -> bool:
-        print("es")
         exp1 = "^([A-Z]\d{8})$"
         exp2 = "^([A-HN-SW]\d{7}[A-J])$"
         exp3 = "^([0-9YZ]\d{7}[A-Z])$"
@@ -193,21 +176,16 @@ class ViesValidator:
         return False
 
     def _eu_vies_check(self, vies_number: str) -> bool:
-        print("eu")
         exp = "^(\d{9})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _fi_vies_check(self, vies_number: str) -> bool:
-        print("fi")
         exp = "^(\d{8})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _fr_vies_check(self, vies_number: str) -> bool:
-        print("fr")
         exp1 = "^(\d{11})$"
         exp2 = "^([A-HJ-NP-Z]\d{10})$"
         exp3 = "^(\d[A-HJ-NP-Z]\d{9})$"
@@ -221,7 +199,6 @@ class ViesValidator:
         return False
 
     def _gb_vies_check(self, vies_number: str) -> bool:
-        print("gb")
         exp1 = "^(\d{9})$"
         exp2 = "^(\d{12})$"
         exp3 = "^(GD\d{3})$"
@@ -235,21 +212,16 @@ class ViesValidator:
         return False
 
     def _hr_vies_check(self, vies_number: str) -> bool:
-        print("hr")
         exp = "^(\d{11})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _hu_vies_check(self, vies_number: str) -> bool:
-        print("hu")
         exp = "^(\d{8})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _ie_vies_check(self, vies_number: str) -> bool:
-        print("ie")
         check_sum = 0
         exp1 = "^(\d{7}[A-W])$"
         exp2 = "^([7-9][A-Z\*\+)]\d{5}[A-W])$"
@@ -280,14 +252,11 @@ class ViesValidator:
         return False
 
     def _it_vies_check(self, vies_number: str) -> bool:
-        print("it")
         exp = "^(\d{11})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _lv_vies_check(self, vies_number: str) -> bool:
-        print("lv")
         exp = "^(\d{11})$"
         r = re.search(exp, vies_number)
         if not bool(r):
@@ -300,7 +269,6 @@ class ViesValidator:
             check_sum = 0
             digits = [int(i) for i in vies_number]
             weights = (9, 1, 4, 8, 3, 10, 2, 5, 7, 6)
-            
             for i in range(0, 10):
                 check_sum += digits[i] * weights[i]
             if (check_sum % 11 == 4) and (vies_number[0] == 9):
@@ -317,28 +285,21 @@ class ViesValidator:
         return False
 
     def _lt_vies_check(self, vies_number: str) -> bool:
-        print("lt")
         exp = "^(\d{9}|\d{12})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _lu_vies_check(self, vies_number: str) -> bool:
-        print("lu")
         exp = "^(\d{8})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _mt_vies_check(self, vies_number: str) -> bool:
-        print("mt")
         exp = "^([1-9]\d{7})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _nl_vies_check(self, vies_number: str) -> bool:
-        print("nl")
         exp1 = "^(\d{9})$"
         exp2 = "^(\d{12})$"
         r1 = bool(re.search(exp1, vies_number))
@@ -348,15 +309,11 @@ class ViesValidator:
         return False
 
     def _no_vies_check(self, vies_number: str) -> bool:
-
-        print("no")
         exp = "^(\d{9})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _pl_vies_check(self, vies_number: str) -> bool:
-        print("pl")
         exp = "^(\d{10})$"
         r = re.search(exp, vies_number)
         if not bool(r):
@@ -367,14 +324,11 @@ class ViesValidator:
         return check_sum == digits[9]
 
     def _pt_vies_check(self, vies_number: str) -> bool:
-        print("pt")
         exp = "^(((([1-3]|5|6)\d)|(45)|(7([0-2]|[4-5]|[7-9]))|(9[0|1|8|9]))(\d{7}$))"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _ro_vies_check(self, vies_number: str) -> bool:
-        print("ro")
         exp = "^([1-9]\d{1,9})$"
         r = re.search(exp, vies_number)
         if not bool(r):
@@ -389,28 +343,21 @@ class ViesValidator:
         return check_sum == int(vies_number[-1])
 
     def _ru_vies_check(self, vies_number: str) -> bool:
-        print("ru")
         exp = "^(\d{10}|\d{12})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _rs_vies_check(self, vies_number: str) -> bool:
-        print("rs")
         exp = "^(\d{9})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _si_vies_check(self, vies_number: str) -> bool:
-        print("si")
         exp = "^([1-9]\d{7})$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
 
     def _sk_vies_check(self, vies_number: str) -> bool:
-        print("sk")
         exp = "^([1-9]\d[2346-9]\d{7})$"
         r = re.search(exp, vies_number)
         if not bool(r):
@@ -419,8 +366,6 @@ class ViesValidator:
         return not vies_number % 11
 
     def _se_vies_check(self, vies_number: str) -> bool:
-        print("se")
         exp = "^(\d{10}01)$"
         r = re.search(exp, vies_number)
-        print(bool(r))
         return bool(r)
